10e198d8f35513167a2ffb7ec2e95b4504c20ee2
masky5859 <mathsuky2406@gmail.com>
2024-06-14 12:08:15 +0900

    init

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/" class="text-blue-500">Home</RouterLink> |
+        <RouterLink to="/about" class="text-blue-500">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is a home page</h1>
+  </div>
+</template>
+
+<style>
+</style>
