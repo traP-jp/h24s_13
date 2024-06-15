bf649ee127514bd869b2543e5d0f64323aca932e
riku_6174 <riku.saso.822@gmail.com>
2024-06-15 13:05:28 +0900

    構成が完成しました

--- client/src/views/PrepareView.vue
+++ client/src/views/PrepareView.vue
@@ -3,16 +3,32 @@
 </script>
 
 <template>
-  
-  <h1>ゲームの説明</h1>
-  <p>
-   traQidを入力してスタートボタンを押すとゲームが始まります。（入力がない場合はランダムで選ばれます）ゲーム画面では選んだ人（以下hogeさんと呼ぶ）とランダムで５人が表示されるので５人をhogeさんとつながりの強い順に並び変えてください。
+  <br><br><br>
+  <h1 class="text-center text-2xl font-bold">ゲームの説明</h1>
+  <br><br>
+  <p class="max-w-2xl mx-auto">
+   traQidを入力してスタートボタンを押すとゲームが始まります。（入力がない場合はランダムで選ばれます）ゲーム画面では選んだ人（以下hogeさんと呼ぶ）とランダムで５人が表示されるので、５人をhogeさんと繋がりの強い順に並び変えてください。
   </p>
-  <label for="traQid">traQid:</label>
-  <input id="traQid" type="text">
+  <p class="max-w-2xl mx-auto">
+    ただし“A さんと B さん繋がりの強さ” := “A さんが B さんの times で発言した数” + “B さんが A さんの times で発言した数”
+  </p>
+  <br><br>
+  <div class="flex justify-center">
+    <label for="traQid" class="">traQIDを入力してください:</label>
+    <input id="traQid" type="text" class="bg-white px-3 text-gray-700 border border-gray-700 rounded">
+  </div>
+  
   <br>
-  <button>ゲームスタート</button>
+  <div class="flex justify-center">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded fucus:outline-none focus:shadow-outline mt-3"
+    >
+      ゲームスタート
+    </button>
+  </div>
   
 </template>
 
-<style></style>
+<style>
+  
+</style>
