adafbeb064643cb42d1e9b86ec2dca3be3f21ba0
Futadaruma <0098yuta@gmail.com>
2024-06-15 10:58:13 +0900

    ボタンの追加

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="about">
-    <h1>This is a home page</h1>
+    <h1>タイトルの名前</h1>
   </div>
+  <button v-on:click="home">ゲームをプレイ</button>
 </template>
 
+<script>
+export default {
+  methods: {
+    home(){
+      this.$router.push('/router')
+    }
+  }
+}
+</script>
 <style>
 </style>
