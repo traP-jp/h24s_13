96cf5de2b781011ccccf3f1f77dc3fbe937c34ec
riku_6174 <riku.saso.822@gmail.com>
2024-06-15 16:19:39 +0900

    列ごとに管理するようにしました

--- client/src/components/ListComponent.vue
+++ client/src/components/ListComponent.vue
@@ -6,29 +6,57 @@
     traPID:string
     name : string
   }
-  const profiles = ref<profile[]>
-  ([{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
-  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
-  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
-  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
-  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
-  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
-  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
-  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
-  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
-  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
-  {traPID:"Series_205",name:"Series_205"}])
+  const profilesA = ref<profile[]>([{traPID:"riku_6174",name:"riku_6174"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"riku_6174",name:"riku_6174"}])
+  const profilesB = ref<profile[]>([{traPID:"masky5859",name:"masky"},{traPID:"masky5859",name:"masky"},{traPID:"masky5859",name:"masky"},{traPID:"masky5859",name:"masky"},{traPID:"masky5859",name:"masky"}])
+  const profilesC = ref<profile[]>([{traPID:"Futadaruma",name:"フタだるま"},{traPID:"Futadaruma",name:"フタだるま"},{traPID:"Futadaruma",name:"フタだるま"},{traPID:"Futadaruma",name:"フタだるま"},{traPID:"Futadaruma",name:"フタだるま"}])
+  const profilesD = ref<profile[]>([{traPID:"toki",name:"とき"},{traPID:"toki",name:"とき"},{traPID:"toki",name:"とき"},{traPID:"toki",name:"とき"},{traPID:"toki",name:"とき"}])
+  const profilesE = ref<profile[]>([{traPID:"Series_205",name:"Series_205"},{traPID:"Series_205",name:"Series_205"},{traPID:"Series_205",name:"Series_205"},{traPID:"Series_205",name:"Series_205"},{traPID:"Series_205",name:"Series_205"}])
 </script>
 
 <template>
   <div>友達の友達リスト</div>
-  <div class="grid grid-rows-5 grid-flow-col">
-    <div v-for="profile in profiles" :key="profile.traPID">
+  <div class="grid grid-rows-1 grid-flow-col">
+    <div>
+      <div v-for="profile in profilesA" :key="profile.traPID" >
         <div class="flex bg-blue-50">
           <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
           <span>{{ profile.name }}</span>
         </div>
       </div>
+    </div>
+    <div>
+      <div v-for="profile in profilesB" :key="profile.traPID">
+        <div class="flex bg-red-50">
+          <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
+          <span>{{ profile.name }}</span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div v-for="profile in profilesC" :key="profile.traPID">
+        <div class="flex bg-green-50">
+          <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
+          <span>{{ profile.name }}</span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div v-for="profile in profilesD" :key="profile.traPID">
+        <div class="flex bg-yellow-50">
+          <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
+          <span>{{ profile.name }}</span>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div v-for="profile in profilesE" :key="profile.traPID">
+        <div class="flex bg-purple-50">
+          <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
+          <span>{{ profile.name }}</span>
+        </div>
+      </div>
+    </div>
   </div>
+    
 </template>
 <style scoped></style>
