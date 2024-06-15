368c01412c09151cade1bdca110f4a7aace2c904
Futadaruma <0098yuta@gmail.com>
2024-06-15 12:40:46 +0900

    仮タイトル画面完成

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="about">
+  <div class="text-8xl aboutw-100 flex justify-center mt-5">
     <h1>タイトルの名前</h1>
   </div>
-  <button v-on:click="home">ゲームをプレイ</button>
+  <div class="flex justify-center items-center h-screen">
+    <button @click="home" class="text-7xl bg-green-400 px-8 py-4">
+      ゲームをプレイ
+    </button>
+  </div>
 </template>
 
-<script>
-export default {
-  methods: {
-    home(){
-      this.$router.push('/router')
-    }
-  }
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function home() {
+  router.push('/router')
 }
 </script>
+
 <style>
 </style>
