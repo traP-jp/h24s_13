ab1d4628b885c15e1f66d082511c77c14ba1c893
riku_6174 <riku.saso.822@gmail.com>
2024-06-15 10:56:44 +0900

    最低限の要素を配置しました

--- client/src/views/PrepareView.vue
+++ client/src/views/PrepareView.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is a prepare page</h1>
-  </div>
+  
+  <h1>ゲームの説明</h1>
+  <p>
+   traQidを入力してスタートボタンを押すとゲームが始まります。（入力がない場合はランダムで選ばれます）ゲーム画面では選んだ人（以下hogeさんと呼ぶ）とランダムで５人が表示されるので５人をhogeさんとつながりの強い順に並び変えてください。
+  </p>
+  <label for="traQid">traQid:</label>
+  <input id="traQid" type="text">
+  <br>
+  <button>ゲームスタート</button>
+  
 </template>
 
 <style></style>
