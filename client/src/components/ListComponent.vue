68821bb9c423d82f93737099e9172b48a3c058e8
riku_6174 <riku.saso.822@gmail.com>
2024-06-15 15:27:02 +0900

    プロフィールリストを作りました

--- client/src/components/ListComponent.vue
+++ client/src/components/ListComponent.vue
@@ -1,7 +1,34 @@
+
+
+<script setup lang="ts">
+  import {ref} from 'vue'
+  interface profile {
+    traPID:string
+    name : string
+  }
+  const profiles = ref<profile[]>
+  ([{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
+  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
+  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
+  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
+  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
+  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
+  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
+  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
+  {traPID:"Series_205",name:"Series_205"},{traPID:"riku_6174",name:"riku_6174"},{traPID:"masky5859",name:"masky"},
+  {traPID:"Futadaruma",name:"フタだるま"},{traPID:"toki",name:"とき"},
+  {traPID:"Series_205",name:"Series_205"}])
+</script>
+
 <template>
-  <div>This is a ListComponent.</div>
+  <div>友達の友達リスト</div>
+  <div class="grid grid-rows-5 grid-flow-col gap-4">
+    <div v-for="profile in profiles" :key="profile.traPID">
+        <div class="flex">
+          <img :src= "'https://q.trap.jp/api/v3/public/icon/'+profile.traPID" alt="アイコン" class="w-6 rounded-full">
+          <span>{{ profile.name }}</span>
+        </div>
+      </div>
+  </div>
 </template>
-
-<script setup lang="ts"></script>
-
 <style scoped></style>
