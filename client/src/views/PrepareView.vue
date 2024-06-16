6fa908761db678a8745fe1d225697ba289ef19f2
Futadaruma <0098yuta@gmail.com>
2024-06-16 11:15:02 +0900

    PerpareViewデザイン調整

--- client/src/views/PrepareView.vue
+++ client/src/views/PrepareView.vue
@@ -26,35 +26,33 @@ const getFivePeople = async (id: string) => {
 </script>
 
 <template>
-  <br /><br /><br />
-  <h1 class="text-center text-2xl font-bold">ゲームの説明</h1>
-  <br /><br />
-  <p class="max-w-2xl mx-auto">
-    traQidを入力してスタートボタンを押すとゲームが始まります。（入力がない場合はランダムで選ばれます）ゲーム画面では選んだ人（以下hogeさんと呼ぶ）とランダムで5人が表示されるので、5人をhogeさんと繋がりの強い順に並び変えてください。
-  </p>
-  <p class="max-w-2xl mx-auto">
-    ただし“A さんと B さん繋がりの強さ” := “A さんが B さんの times で発言した数” + “B さんが A
-    さんの times で発言した数”
-  </p>
-  <br /><br />
-  <div class="flex justify-center">
-    <label for="traQid" class="">traQIDを入力してください:</label>
-    <input
-      id="traQid"
-      type="text"
-      v-model="traQID"
-      class="bg-white px-3 text-gray-700 border border-gray-700 rounded"
-    />
-  </div>
+  <div class="container mx-auto app">
+    <h1 class="text-4xl text-center h-20 text-left font-bold">ゲームの説明</h1>
 
-  <br />
-  <div class="flex justify-center">
-    <button
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded fucus:outline-none focus:shadow-outline mt-3"
-      @click="getFivePeople(traQID)"
-    >
-      ゲームスタート
-    </button>
+      <p class="max-w-2xl mx-auto text-left font-bold h-20">
+        traQidを入力してスタートボタンを押すとゲームが始まります。（入力がない場合はランダムで選ばれます）ゲーム画面では選んだ人（以下hogeさんと呼ぶ）とランダムで5人が表示されるので、5人をhogeさんと繋がりの強い順に並び変えてください。
+      </p>
+      <p class="max-w-2xl mx-auto text-left font-bold h-20">
+       ただし“AさんとBさん繋がりの強さ” := “AさんがBさんの times で発言した数” + “BさんがAさんの times で発言した数”
+      </p>
+      <div class="flex justify-center">
+        <label for="traQid" class="font-bold">traQIDを入力してください:</label>
+        <input
+         id="traQid"
+         type="text"
+         v-model="traQID"
+         class="bg-white px-3 text-gray-700 border border-gray-700 rounded"
+         />
+      </div>
+      <br/>
+    <div class="flex justify-center">
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-5 rounded fucus:outline-none focus:shadow-outline mt-3"
+        @click="getFivePeople(traQID)"
+        >
+          ゲームスタート
+      </button>
+    </div>
   </div>
 </template>
 
