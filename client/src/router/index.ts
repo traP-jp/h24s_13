3f55beba6eae769491daab80f5c1529233b72f99
masky5859 <mathsuky2406@gmail.com>
2024-06-16 14:05:29 +0900

    fix/index.ts

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,7 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import PrepareView from '../views/PrepareView.vue'
-import WebView from '../components/WebComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,20 +6,12 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/prepare',
       name: 'prepare',
-      component: PrepareView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/PrepareView.vue')
     },
     {
       path: '/game',
